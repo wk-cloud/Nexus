0756d0ca0eb68ae5be202e5c9f643cf8c1752d1e
wk <3052236335@qq.com>
2026-04-14 10:49:55 +0800

    refactor: 优化用户名工具类

--- nexus-common/nexus-common-core/src/main/java/com/nexus/common/core/utils/UserNameUtils.java
+++ nexus-common/nexus-common-core/src/main/java/com/nexus/common/core/utils/UserNameUtils.java
@@ -18,55 +18,82 @@ public class UserNameUtils {
 
     /**
      * 生成用户名
-     *
+     * 组成部分：前缀 + 指定位数的数字和英文编码
      * @param accountNumber 账号
      * @param begin         开始
      * @param end           结束
      * @return {@link String}
      */
     public static String generate(String accountNumber, Integer begin, Integer end) {
-        String uuid = UUID.randomUUID().toString().replaceAll("-", "").substring(begin, end);
-        String userName = null;
-        if (VerificationUtils.isEmail(accountNumber)) {
-            userName = "m0_" + uuid;
-        } else if (VerificationUtils.isQQ(accountNumber)) {
-            userName = "qq_" + uuid;
-        }
-        return userName;
+        String code = UUID.randomUUID().toString().replaceAll("-", "").substring(begin, end);
+        return addPrefix(accountNumber, code);
     }
 
     /**
      * 生成用户名
-     *
+     * 组成部分：前缀 + 九位纯数字编码
+     * @param accountNumber 账号
+     * @return {@link String}
+     */
+    public static String generate(String accountNumber) {
+        String code = UUID.randomUUID().toString().replaceAll("[^0-9]", "").substring(0,9);
+        return addPrefix(accountNumber, code);
+    }
+
+    /**
+     * 生成用户名
+     * 组成部分：前缀 + 指定位数的数字和英文编码
      * @param loginType 登录类型
      * @param begin     开始
      * @param end       结束
      * @return {@link String}
      */
     public static String generate(Integer loginType,Integer begin,Integer end){
-        String uuid = UUID.randomUUID().toString().replaceAll("-", "").substring(begin, end);
+        String code = UUID.randomUUID().toString().replaceAll("-", "").substring(begin, end);
+        return addPrefix(loginType, code);
+    }
+
+    /**
+     * 生成用户名
+     * 组成部分：前缀 + 九位纯数字编码
+     * @param loginType 登录类型
+     * @return {@link String}
+     */
+    public static String generate(Integer loginType){
+        String code = UUID.randomUUID().toString().replaceAll("[^0-9]", "").substring(0,9);
+        return addPrefix(loginType, code);
+    }
+
+    /**
+     * 添加前缀
+     *
+     * @param loginType 登录类型
+     * @param code      编码
+     * @return {@link String }
+     */
+    private static String addPrefix(Integer loginType, String code) {
         String userName = null;
-        if (LoginTypeEnum.EMAIL.getCode().equals(loginType)) {
-            userName = "m0_" + uuid;
+        if (LoginTypeEnum.PASSWORD.getCode().equals(loginType) || LoginTypeEnum.PASSWORD_FREE.getCode().equals(loginType)) {
+            userName = "m0_" + code;
         } else if (LoginTypeEnum.QQ.getCode().equals(loginType)) {
-            userName = "qq_" + uuid;
+            userName = "qq_" + code;
         }
         return userName;
     }
 
     /**
-     * 生成用户名
+     * 添加前缀
      *
-     * @param loginType 登录类型
-     * @return {@link String}
+     * @param accountNumber 账号
+     * @param code      编码
+     * @return {@link String }
      */
-    public static String generate(Integer loginType){
-        String uuid = UUID.randomUUID().toString().replaceAll("[^0-9]", "").substring(0,9);
+    private static String addPrefix(String accountNumber, String code) {
         String userName = null;
-        if (LoginTypeEnum.EMAIL.getCode().equals(loginType)) {
-            userName = "m0_" + uuid;
-        } else if (LoginTypeEnum.QQ.getCode().equals(loginType)) {
-            userName = "qq_" + uuid;
+        if (VerificationUtils.isEmail(accountNumber)) {
+            userName = "m0_" + code;
+        } else if (VerificationUtils.isQQ(accountNumber)) {
+            userName = "qq_" + code;
         }
         return userName;
     }

c5f0ef0d00f5c392cbf6d0496d547b81c8589e95
wk <3052236335@qq.com>
2026-06-20 16:03:24 +0800

    refactor: qq登录配置优化

--- nexus-services/nexus-services-auth/src/main/java/com/nexus/auth/config/QQLoginConfig.java
+++ nexus-services/nexus-services-auth/src/main/java/com/nexus/auth/config/QQLoginConfig.java
@@ -1,13 +1,10 @@
 package com.nexus.auth.config;
-
 import cn.hutool.http.HttpUtil;
 import cn.hutool.json.JSONUtil;
 import com.alibaba.fastjson2.JSON;
+import com.nexus.auth.config.properties.QqLoginProperties;
 import com.nexus.common.core.utils.CollectionUtils;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import org.springframework.boot.context.properties.ConfigurationProperties;
+import jakarta.annotation.Resource;
 import org.springframework.context.annotation.Configuration;
 
 import java.util.HashMap;
@@ -21,17 +18,25 @@ import java.util.Map;
  * @date 2023/2/19 14:06
  */
 @Configuration
-@ConfigurationProperties(prefix = "qq")
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
 public class QQLoginConfig {
 
-    private String clientId;
-    private String clientSecret;
-    private String grantType;
-    private String redirectUri;
-    private String fmt;
+    /**
+     * 获取 access_token 的URL
+     */
+    private static final String ACCESS_TOKEN_URL = "https://graph.qq.com/oauth2.0/token";
+
+    /**
+     * 获取 openid 的URL
+     */
+    private static final String OPEN_ID_URL = "https://graph.qq.com/oauth2.0/me";
+
+    /**
+     * 获取用户信息的URL
+     */
+    private static final String USER_INFO_URL = "https://graph.qq.com/user/get_user_info";
+
+    @Resource
+    private QqLoginProperties qqLoginProperties;
 
     /**
      * 获取用户信息
@@ -39,16 +44,16 @@ public class QQLoginConfig {
      * @param code 代码
      * @return {@link Map}<{@link String},{@link Object}>
      */
-    public Map<String,Object> getUserInfoMap(String code){
+    public Map<String,Object> getUserInfo(String code){
         HashMap<String, Object> paramMap = new HashMap<>(CollectionUtils.initialCapacity(6));
-        paramMap.put("client_id", clientId);
-        paramMap.put("client_secret", clientSecret);
-        paramMap.put("grant_type", grantType);
-        paramMap.put("redirect_uri", redirectUri);
-        paramMap.put("fmt", fmt);
+        paramMap.put("client_id", qqLoginProperties.getClientId());
+        paramMap.put("client_secret", qqLoginProperties.getClientSecret());
+        paramMap.put("grant_type", qqLoginProperties.getGrantType());
+        paramMap.put("redirect_uri", qqLoginProperties.getRedirectUri());
+        paramMap.put("fmt", qqLoginProperties.getFmt());
         paramMap.put("code", code);
         // 1. 获取 access_token
-        String accessSource = HttpUtil.get("https://graph.qq.com/oauth2.0/token", paramMap);
+        String accessSource = HttpUtil.get(ACCESS_TOKEN_URL, paramMap);
         Map<String,Object> accessTokenMap = JSONUtil.parseObj(accessSource);
         Object accessToken = accessTokenMap.get("access_token");
 
@@ -56,15 +61,15 @@ public class QQLoginConfig {
         paramMap.clear();
         paramMap.put("access_token", accessToken);
         paramMap.put("fmt", "json");
-        String openIdSource = HttpUtil.get("https://graph.qq.com/oauth2.0/me", paramMap);
+        String openIdSource = HttpUtil.get(OPEN_ID_URL, paramMap);
         Map<String,Object> openIdMap = JSONUtil.parseObj(openIdSource);
         String openid = (String) openIdMap.get("openid");
         // 3. 获取用户信息
         paramMap.clear();
         paramMap.put("access_token", accessToken);
-        paramMap.put("oauth_consumer_key", clientId);
+        paramMap.put("oauth_consumer_key", qqLoginProperties.getClientId());
         paramMap.put("openid", openid);
-        String userInfoSource = HttpUtil.get("https://graph.qq.com/user/get_user_info", paramMap);
+        String userInfoSource = HttpUtil.get(USER_INFO_URL, paramMap);
         Map<String,Object> userInfoMap = (Map<String, Object>) JSON.parse(userInfoSource);
         userInfoMap.put("openId",openid);
         return userInfoMap;

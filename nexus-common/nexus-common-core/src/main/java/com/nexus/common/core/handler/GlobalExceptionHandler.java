5d4b58f5271b67d1e85219154803423a3c48d78e
wk <3052236335@qq.com>
2026-09-22 15:07:43 +0800

    refactor: 优化全局异常处理

--- nexus-common/nexus-common-core/src/main/java/com/nexus/common/core/handler/GlobalExceptionHandler.java
+++ nexus-common/nexus-common-core/src/main/java/com/nexus/common/core/handler/GlobalExceptionHandler.java
@@ -5,6 +5,7 @@ import com.nexus.common.core.exception.LimitAccessException;
 import com.nexus.common.core.exception.ServiceException;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.tomcat.util.http.fileupload.impl.FileSizeLimitExceededException;
+import org.springframework.core.annotation.Order;
 import org.springframework.validation.BindException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -12,66 +13,14 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 /**
  * 全局异常处理器
+ *
  * @author wk
  * @date 2025/09/14
  */
 @Slf4j
+@Order(1) // 因为全局异常处理与Shiro异常处理都有@ExceptionHandler注解，所以需要指定顺序，防止Shiro异常处理被全局异常处理拦截
 @RestControllerAdvice
 public class GlobalExceptionHandler {
-
-    /**
-     * 处理系统异常
-     *
-     * @param e e
-     * @return {@link Result}
-     */
-    @ExceptionHandler({NullPointerException.class,RuntimeException.class})
-    public Result<Void> systemExceptionHandle(Exception e){
-        log.error("系统异常", e);
-        return Result.fail("系统出现异常");
-    }
-
-
-    /**
-     * 处理超出文件大小限制异常
-     *
-     * @param e e
-     * @return {@link Result}
-     */
-    @ExceptionHandler(FileSizeLimitExceededException.class)
-    public Result<Void> fileSizeLimitExceededExceptionHandle(Exception e){
-        log.error("上传文件大小超过限制", e);
-        return Result.fail("上传失败，失败原因：单次上传不能超过100MB");
-    }
-
-
-    /**
-     * 处理参数校验异常
-     *
-     * @param bindException 绑定异常
-     * @return {@link Result}
-     */
-    @ExceptionHandler(BindException.class)
-    public Result<Void> bindExceptionHandle(BindException bindException){
-        FieldError fieldError = bindException.getFieldError();
-        if (fieldError == null) {
-            log.error("BindException 的 FieldError 为 null");
-            return Result.fail("参数校验失败");
-        }
-        return Result.fail(fieldError.getDefaultMessage());
-    }
-
-    /**
-     *  处理限制访问异常
-     *
-     * @param e e
-     * @return {@link Result}
-     */
-    @ExceptionHandler(LimitAccessException.class)
-    public Result<Void> limitAccessExceptionHandle(LimitAccessException e){
-        log.error("限制访问", e);
-        return Result.fail(e.getMessage());
-    }
 
     /**
      * 处理服务异常
@@ -80,9 +29,61 @@ public class GlobalExceptionHandler {
      * @return {@link Result}
      */
     @ExceptionHandler(ServiceException.class)
-    public Result<Void> serviceExceptionHandle(ServiceException e){
-        log.error("服务异常", e);
-        return Result.fail(e.getCode(),e.getMessage());
+    public Result<Void> serviceExceptionHandle(ServiceException e) {
+        log.error("====> 服务异常", e);
+        return Result.fail(e.getCode(), e.getMessage());
+    }
+
+    /**
+     * 处理参数校验异常
+     *
+     * @param bindException 绑定异常
+     * @return {@link Result}
+     */
+    @ExceptionHandler(BindException.class)
+    public Result<Void> bindExceptionHandle(BindException bindException) {
+        FieldError fieldError = bindException.getFieldError();
+        if (fieldError == null) {
+            log.error("====> BindException 的 FieldError 为 null");
+            return Result.fail("参数校验失败");
+        }
+        return Result.fail(fieldError.getDefaultMessage());
+    }
+
+    /**
+     * 处理限制访问异常
+     *
+     * @param e e
+     * @return {@link Result}
+     */
+    @ExceptionHandler(LimitAccessException.class)
+    public Result<Void> limitAccessExceptionHandle(LimitAccessException e) {
+        log.error("====> 限制访问", e);
+        return Result.fail(e.getMessage());
+    }
+
+    /**
+     * 处理超出文件大小限制异常
+     *
+     * @param e e
+     * @return {@link Result}
+     */
+    @ExceptionHandler(FileSizeLimitExceededException.class)
+    public Result<Void> fileSizeLimitExceededExceptionHandle(Exception e) {
+        log.error("====> 上传文件大小超过限制", e);
+        return Result.fail("上传失败，失败原因：单次上传不能超过100MB");
+    }
+
+    /**
+     * 处理系统异常
+     *
+     * @param e e
+     * @return {@link Result}
+     */
+    @ExceptionHandler({NullPointerException.class, RuntimeException.class})
+    public Result<Void> systemExceptionHandle(Exception e) {
+        log.error("====> 系统异常", e);
+        return Result.fail("系统出现异常");
     }
 
 }

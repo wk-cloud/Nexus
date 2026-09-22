6464aec78ec739500ce6fe146bb9a7f0888af71f
wk <3052236335@qq.com>
2026-09-22 15:09:08 +0800

    refactor: 优化接口文档配置类

--- nexus-common/nexus-common-doc/src/main/java/com/nexus/common/doc/config/NexusSwaggerConfig.java
+++ nexus-common/nexus-common-doc/src/main/java/com/nexus/common/doc/config/NexusSwaggerConfig.java
@@ -3,8 +3,10 @@ package com.nexus.common.doc.config;
 
 import io.swagger.v3.oas.models.ExternalDocumentation;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -15,14 +17,18 @@ import org.springframework.context.annotation.Configuration;
  * @date 2022/7/20
  */
 @Configuration
-public class SwaggerConfig {
+public class NexusSwaggerConfig {
 
+    @ConditionalOnMissingBean
     @Bean
     public OpenAPI customerOpenApi() {
         return new OpenAPI().info(new Info()
                         .title("nexus接口文档")
                         .description("基于 SpringBoot3 + Vue3 的前后端分离的web开发模版")
                         .version("1.0.0")
+                        .contact(new Contact()
+                                .name("wk")
+                                .url("https://wk-blog.vip"))
                         .license(new License()
                                 .name("Apache 2.0")
                                 .url("https://www.apache.org/licenses/LICENSE-2.0")))

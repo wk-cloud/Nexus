0a14d612d23632722869b1c2cb6f99657fc1ada2
wk <3052236335@qq.com>
2026-09-22 15:07:12 +0800

    refactor: 优化Shiro异常处理

--- nexus-common/nexus-common-shiro/src/main/java/com/nexus/common/shiro/handler/ShiroExceptionHandler.java
+++ nexus-common/nexus-common-shiro/src/main/java/com/nexus/common/shiro/handler/ShiroExceptionHandler.java
@@ -9,6 +9,7 @@ import com.nexus.common.core.enums.HttpCodeEnum;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.shiro.authz.AuthorizationException;
 import org.apache.shiro.authz.UnauthorizedException;
+import org.springframework.core.annotation.Order;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -19,6 +20,7 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
  * @date 2026/4/7 11:16
  */
 @Slf4j
+@Order(-1) // 因为全局异常处理与Shiro异常处理都有@ExceptionHandler注解，所以需要指定顺序，防止Shiro异常处理被全局异常处理拦截
 @RestControllerAdvice
 public class ShiroExceptionHandler {
 
@@ -28,21 +30,22 @@ public class ShiroExceptionHandler {
      * @param e e
      * @return {@link Result}
      */
-    @ExceptionHandler({SignatureVerificationException.class,AlgorithmMismatchException.class,TokenExpiredException.class, JWTDecodeException.class})
-    public Result<Void> verificationOrAlgorithmMismatchExceptionHandle(Exception e){
-        log.error("token 异常", e);
-        return Result.fail(HttpCodeEnum.TOKEN_EXPIRED.getCode(),"登录信息已经失效，请重新登录");
+    @ExceptionHandler({SignatureVerificationException.class, AlgorithmMismatchException.class, TokenExpiredException.class, JWTDecodeException.class})
+    public Result<Void> verificationOrAlgorithmMismatchExceptionHandle(Exception e) {
+        log.error("====> token 异常", e);
+        return Result.fail(HttpCodeEnum.TOKEN_EXPIRED.getCode(), "登录信息已经失效，请重新登录");
     }
 
 
     /**
      * 处理shiro异常
+     *
      * @param e e
      * @return {@link Result}
      */
     @ExceptionHandler({UnauthorizedException.class, AuthorizationException.class})
-    public Result<Void> unAuthorizedExceptionHandle(Exception e){
-        log.error("shiro 异常", e);
+    public Result<Void> unAuthorizedExceptionHandle(Exception e) {
+        log.error("====> Shiro 异常", e);
         return Result.fail("权限不足");
     }
 }
